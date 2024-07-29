7a391a9cc94b6896390f1770b2aab042fe61c1c8
Manobalan04 <manobalans2001@outlook.com>
2024-07-30 00:07:07 +0530

    first commit

---
+++ emp_frontend/empft/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListEmployeeComponents from './Components/ListEmployeeComponents';
+
+function App() {
+  return (
+    <div className="Container">
+      <ListEmployeeComponents/>
+    </div>
+  );
+}
+
+export default App;
